9823cf714723272f241f492e550427c6458d3363
Gabriel Soares da Silva Góes <gabriel_soares1125@outlook.com>
2023-07-13 20:33:17 -0300

    button definitions

--- amortization-comparator_app/src-tauri/src/main.rs
+++ amortization-comparator_app/src-tauri/src/main.rs
@@ -7,6 +7,17 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+use tauri::{CustomMenuItem, Menu, MenuItem, SubMenu};
+
+let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+let close = CustomMenuItem::new("close".to_string(), "Close");
+let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
+let menu = Menu::new()
+    .add_native_item(MenuItem::Copy)
+    .add_item(CustomMenuItem::new("hide", "Hide"))
+    .add_submenu(submenu);
+
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
